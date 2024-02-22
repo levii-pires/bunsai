a1c633c0ae482f20611979e28334c0cb13e73f9d
Levi Pires <mateus.amare@gmail.com>
2024-02-22 17:43:12 -0300

    update sass loader

--- loaders/sass.ts
+++ loaders/sass.ts
@@ -4,9 +4,11 @@ import { compile, Options } from "sass";
 export default function getSassLoader(
   options?: Options<"sync">
 ): LoaderInitiator {
-  return () => (filePath) => {
-    return new Response(compile(filePath, options).css, {
-      headers: { "Content-Type": "text/css; charset=utf-8" },
-    });
+  return () => {
+    return async (filePath) => {
+      return new Response(compile(filePath, options).css, {
+        headers: { "Content-Type": "text/css; charset=utf-8" },
+      });
+    };
   };
 }
