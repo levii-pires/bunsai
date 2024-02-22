2e1bb8795015af98c29174d92a37b51c6d7e0fd9
Levi Pires <mateus.amare@gmail.com>
2024-02-22 17:45:05 -0300

    implement nunjucks test

--- tests/loaders/nunjucks.test.ts
+++ tests/loaders/nunjucks.test.ts
@@ -1,3 +1,28 @@
-import { describe, expect, it } from "bun:test";
+import { describe, expect, it, afterAll } from "bun:test";
+import getNunjucksLoader from "../../loaders/nunjucks";
+import BunSai from "../..";
 
-describe("Nunjucks Loader", () => {});
+const { loaderInit } = getNunjucksLoader();
+
+const { fetch } = new BunSai({
+  loaders: { ".njk": loaderInit },
+  dir: "./tests/pages",
+});
+
+const server = Bun.serve({ fetch });
+
+afterAll(() => {
+  server.unref();
+  server.stop();
+});
+
+describe("Nunjucks Loader", () => {
+  it("should load nunjucks files", async () => {
+    const response = await server.fetch(
+      new Request("https://bun.test/nunjucks")
+    );
+
+    expect(response.headers.get("content-type")).toStartWith("text/html");
+    expect(await response.text()).toInclude("https://bun.test/nunjucks");
+  });
+});
