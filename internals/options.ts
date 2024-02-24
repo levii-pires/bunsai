f3146ff0d13632a1c8f4b03dc574f3548e35c9ce
Levi Pires <mateus.amare@gmail.com>
2024-02-24 18:18:16 -0300

    change default dev resolution

--- internals/options.ts
+++ internals/options.ts
@@ -3,7 +3,7 @@ import type { ResolvedBunSaiOptions, BunSaiOptions } from "../types";
 export function resolveOptions(options: BunSaiOptions): ResolvedBunSaiOptions {
   return {
     assetPrefix: options.assetPrefix || "",
-    dev: options.dev || false,
+    dev: options.dev ?? process.env.NODE_ENV !== "production",
     dir: options.dir || "./pages",
     loaders: options.loaders || {},
     origin: options.origin || "",
