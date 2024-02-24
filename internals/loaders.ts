fe606bd24a67e8253b37cd2a88aedb1632f09e26
Levi Pires <mateus.amare@gmail.com>
2024-02-24 18:18:48 -0300

    updates

--- internals/loaders.ts
+++ internals/loaders.ts
@@ -1,6 +1,9 @@
 import type BunSai from "..";
 import type { Extname, LoaderMap } from "../types";
 
+/**
+ * This function is used by Bunsai's constructor to initiate the loaders.
+ */
 export function initLoaders(bunsai: BunSai) {
   const { options, routeLoaders } = bunsai;
 
--- internals/static.ts
+++ internals/static.ts
@@ -1,10 +1,13 @@
 import type BunSai from "..";
 import { StaticLoaderInit } from "../loaders";
 
+/**
+ * This function is used by BunSai's constructor to resolve the `staticFiles` option.
+ */
 export function getStatic(bunsai: BunSai) {
   const { options, routeLoaders } = bunsai;
 
-  if (options.staticFiles.length == 0) return {};
+  if (options.staticFiles.length == 0) return;
 
   const loader = StaticLoaderInit(options);
 
