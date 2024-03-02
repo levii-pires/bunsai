006f2eff77bbef499c159f27a45b0f5d8f66ead1
Levi Pires <leviapires16@gmail.com>
2024-03-02 02:15:11 -0300

    add html-rewriter middleware

---
+++ middlewares/html-rewriter.ts
@@ -0,0 +1,26 @@
+import type { MiddlewareRunnerWithThis } from "../types";
+import type { Server, MatchedRoute } from "bun";
+import { Middleware } from "../internals/middleware";
+
+export default class Rewriter extends Middleware<"response"> {
+  name = "@builtin.html-rewriter";
+  runsOn = "response" as const;
+
+  constructor(public rewriter: HTMLRewriter) {
+    super();
+  }
+
+  protected $runner: MiddlewareRunnerWithThis<
+    {
+      response: Response;
+      request: Request;
+      server: Server;
+      route: MatchedRoute;
+    },
+    this
+  > = function ({ response }) {
+    if (!response.headers.get("content-type")?.startsWith("text/html")) return;
+
+    return this.rewriter.transform(response);
+  };
+}
