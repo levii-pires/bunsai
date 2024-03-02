4df7a754f44122613e3aaf54d815ff0a55577916
Levi Pires <leviapires16@gmail.com>
2024-03-02 01:42:03 -0300

    updates

--- tests/pages/.svelte2.svelte
+++ tests/pages/.svelte2.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+    <meta name="description" content="desc">
+</svelte:head>
+
 <div>
+ 
     testes
 </div>
--- tests/pages/svelte.svelte
+++ tests/pages/svelte.svelte
@@ -3,9 +3,14 @@ import Svelte2 from './.svelte2.svelte'
 
 </script>
 
-<div>
-    <p>
-        Testing
-        <Svelte2/>
-    </p>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <Svelte2/>
+</body>
+</html>
