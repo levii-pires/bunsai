4f0ef2373dfd72960a71a25b40dfdfff4440bcd8
mateus-p <leviapires16@gmail.com>
2024-03-04 19:55:30 -0300

    update recommended

--- recommended.ts
+++ recommended.ts
@@ -1,41 +1,56 @@
-import type { Extname, Recommended, RecommendedOpts } from "./types";
-import { ModuleLoaderInit } from "./loaders";
-import getNunjucksLoader from "./loaders/nunjucks";
-import getSassLoader from "./loaders/sass";
+import type { Recommended, RecommendedOpts } from "./types";
+import ModuleLoader from "./loaders/module";
+import NunjucksLoader from "./loaders/nunjucks";
+import SassLoader from "./loaders/sass";
 import DDOS from "./middlewares/ddos";
 import CORS from "./middlewares/cors";
+import BunSai from ".";
+import WebLoader from "./loaders/web";
 
-export default function getRecommended(opts?: RecommendedOpts): Recommended {
-  const nunjucks = getNunjucksLoader(opts?.nunjucks?.options);
+export default async function useRecommended(
+  opts?: RecommendedOpts
+): Promise<Recommended> {
+  const nunjucks = new NunjucksLoader(opts?.nunjucks);
 
   const { preflight, response } = CORS(opts?.middlewares?.cors);
 
-  return {
-    get nunjucksEnv() {
-      return nunjucks.env;
-    },
+  const bunsai = await BunSai.init({
+    ...opts?.bunsai,
+    loaders: [
+      new ModuleLoader(),
+      nunjucks,
+      new SassLoader(opts?.sass),
+      new WebLoader(),
+    ],
+    staticFiles: [
+      ".html",
+      ".css",
+      ".js",
+      ".json",
+      ".txt",
+      ".webp",
+      ".gif",
+      ".mp4",
+      ".mov",
+      ".ogg",
+      ".mp3",
+      ".aac",
+      ".ico",
+      ".ttf",
+      ".otf",
+      ".woff",
+      ".woff2",
+    ],
+    middlewares: [new DDOS(opts?.middlewares?.ddos), preflight, response],
+  });
 
+  return {
     nunjucks: {
       env() {
         return nunjucks.env;
       },
     },
 
-    loaders: {
-      ".ts": ModuleLoaderInit,
-      ".tsx": ModuleLoaderInit,
-      ".njk": nunjucks.loaderInit,
-      ".scss": getSassLoader(opts?.sass?.options),
-    },
-
-    // prettier-ignore
-    staticFiles: [
-      ".html", ".css", ".js", ".json", ".txt",
-      ".webp", ".gif", ".mp4", ".mov", ".ogg",
-      ".mp3", ".aac", ".ttf", ".otf", ".woff",
-      ".woff2",
-    ] as Extname[],
-
-    middlewares: [new DDOS(opts?.middlewares?.ddos), preflight, response],
+    bunsai,
   };
 }
