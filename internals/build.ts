8d1afb28f619a97efb9089bf16e6389d33c6fa1d
mateus-p <leviapires16@gmail.com>
2024-03-05 20:00:57 -0300

    continue implementing build

--- internals/build.ts
+++ internals/build.ts
@@ -1,6 +1,5 @@
 import type { Extname } from "../types";
 import type BunSai from "..";
-import type { BuildConfig } from "bun";
 import { dirname, extname, relative, resolve } from "path";
 import FSCache from "./fsCache";
 import FilenameParser from "./filename";
@@ -9,6 +8,8 @@ import { rm } from "fs/promises";
 const buildFolder = "bunsai-build";
 
 export async function build(bunsai: BunSai) {
+  console.time("BunSai build");
+
   console.log("Clearing build folder");
 
   await rm(buildFolder, { force: true, recursive: true });
@@ -18,6 +19,7 @@ export async function build(bunsai: BunSai) {
   const files = Object.values(bunsai.router.routes);
 
   const bundleEntries: string[] = [];
+  const moduleEntries: string[] = [];
 
   const cache = await FSCache.init("build", "@bunsai");
 
@@ -43,23 +45,11 @@ export async function build(bunsai: BunSai) {
 
       switch (res.serve) {
         case "bundle": {
-          const cacheOut = await cache.write(outPath, res.content);
-
-          const { logs, outputs, success } = await Bun.build({
-            ...res.bundleConfig,
-            target: "browser",
-            splitting: false,
-            minify: true,
-            outdir: undefined,
-            entrypoints: [cacheOut],
-          });
-
-          logs.forEach((l) => console.log(l));
-
-          if (success) {
-            bundleEntries.push(await cache.write(outPath, outputs[0]));
-          }
-
+          bundleEntries.push(await cache.write(outPath, res.content));
+          break;
+        }
+        case "module": {
+          moduleEntries.push(await cache.write(outPath, res.content));
           break;
         }
         default: {
@@ -75,15 +65,35 @@ export async function build(bunsai: BunSai) {
     const { logs, outputs } = await Bun.build({
       entrypoints: bundleEntries,
       splitting: true,
+      target: "browser",
       outdir: buildFolder,
+      minify: true,
+    });
+
+    logs.forEach((l) => console.log(l));
+    for (const out of outputs) {
+      const outPath = resolve(buildFolder, out.path);
+      console.log("\t", outPath);
+    }
+  }
+
+  if (moduleEntries.length > 0) {
+    const { logs, outputs } = await Bun.build({
+      entrypoints: moduleEntries,
+      splitting: true,
+      target: "bun",
+      outdir: buildFolder,
+      naming: { chunk: ".module-[name]-[hash].[ext]" },
     });
 
     logs.forEach((l) => console.log(l));
 
     for (const out of outputs) {
       const outPath = resolve(buildFolder, out.path);
-      await Bun.write(outPath, out);
       console.log("\t", outPath);
     }
   }
+
+  console.log();
+  console.timeEnd("BunSai build");
 }
