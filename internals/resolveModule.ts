ea68f38943ed908fa77e9efea490c1ca558d5741
Levi Pires <leviapires16@gmail.com>
2024-03-14 02:55:07 -0300

    updates

--- internals/resolveModule.ts
+++ internals/resolveModule.ts
@@ -1,7 +1,7 @@
 import { resolve } from "path";
 
-export function resolveModule(id: string, source: string): string {
-  return import.meta.resolveSync(
+export function resolveModule(id: string, source: string) {
+  return import.meta.resolve(
     id.replace(/^\$(\w*)?\//, "bunsai/$1/"),
     resolve(source)
   );
--- tests/bunsai.config.ts
+++ tests/bunsai.config.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from "bunsai";
 const options: UserConfig = {
   middlewares: ["$middlewares/ddos.ts"],
   staticFiles: [".html"],
-  loaders: ["$loaders/nunjucks.ts"],
+  loaders: ["$loaders/nunjucks", "$loaders/stylus"],
 };
 
 export default options;
