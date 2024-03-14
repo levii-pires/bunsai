8b67dc3b32c3d72aeba94200d5807a0bad68ce36
Levi Pires <leviapires16@gmail.com>
2024-03-14 02:54:34 -0300

    rename userConf2Options.ts -> evalUserConfig.ts

--- internals/evalUserConfig.ts
+++ internals/evalUserConfig.ts
@@ -10,39 +10,53 @@ interface EvalDeclArgs {
 }
 
 async function evalDecl(args: EvalDeclArgs, userConfigFilePath: string) {
-  const item = (await import(resolveModule(args.path, userConfigFilePath)))
-    .default;
+  const module = await resolveModule(args.path, userConfigFilePath);
+  const item = (await import(module)).default;
 
   args.target.push(new item(args.options));
+
+  return module;
 }
 
-export async function userConf2Options(
+export interface UserConfigEval {
+  options: BunSaiOptions;
+  modules: string[];
+}
+
+export async function evalUserConfig(
   config: UserConfig,
   userConfigFilePath: string
-): Promise<BunSaiOptions> {
+): Promise<UserConfigEval> {
   const loaders: Loader[] = [];
   const middlewares: (AllMiddlewares | MiddlewareCollection)[] = [];
+  const modules: string[] = [];
 
   if (config.loaders) {
     for (const loader of config.loaders) {
       if (typeof loader == "string") {
-        await evalDecl({ path: loader, target: loaders }, userConfigFilePath);
+        modules.push(
+          await evalDecl({ path: loader, target: loaders }, userConfigFilePath)
+        );
 
         continue;
       }
 
       const [path, options] = loader;
 
-      await evalDecl({ path, options, target: loaders }, userConfigFilePath);
+      modules.push(
+        await evalDecl({ path, options, target: loaders }, userConfigFilePath)
+      );
     }
   }
 
   if (config.middlewares) {
     for (const middleware of config.middlewares) {
       if (typeof middleware == "string") {
-        await evalDecl(
-          { path: middleware, target: middlewares },
-          userConfigFilePath
+        modules.push(
+          await evalDecl(
+            { path: middleware, target: middlewares },
+            userConfigFilePath
+          )
         );
 
         continue;
@@ -50,16 +64,21 @@ export async function userConf2Options(
 
       const [path, options] = middleware;
 
-      await evalDecl(
-        { path, options, target: middlewares },
-        userConfigFilePath
+      modules.push(
+        await evalDecl(
+          { path, options, target: middlewares },
+          userConfigFilePath
+        )
       );
     }
   }
 
   return {
-    ...config,
-    loaders,
-    middlewares,
+    options: {
+      ...config,
+      loaders,
+      middlewares,
+    },
+    modules,
   };
 }
